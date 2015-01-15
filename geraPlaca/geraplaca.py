1927ecf3ccd4c00f678c1dd8896eeb46fe4313b1
obelonave <wendeurick@gmail.com>
2015-01-15 09:17:53 -0200

    Create geraplaca.py

---
+++ geraPlaca/geraplaca.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+def main():
+
+    if len(sys.argv) > 1:
+        
+        placas = open(sys.argv[1], "w")
+
+        for L1 in xrange(65, 91):
+            for L2 in xrange(65, 91):
+                for L3 in xrange(65, 91):
+                    for N in xrange(0, 10000):
+                        placas.write("%c%c%c-%04d\n" %(L1, L2, L3, N));
+
+        placas.close()
+
+    else:
+        print "Informe um nome para o arquivo."                    
+
+if __name__ == '__main__':
+    main()
